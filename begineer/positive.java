c6ffb2b2e26f2d101e2690210b0d2dd20d75ef83
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2019-03-19 14:53:45 +0530

    Second Commit

---
+++ begineer/positive.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class positive{
+	public static void main(String[] args){
+		Scanner cin=new Scanner(System.in);
+		int n=cin.nextInt();
+		if(n>=0){
+			System.out.println("Positive");
+		}
+		else{
+			System.out.println("Negative");
+		}
+	}
+}
