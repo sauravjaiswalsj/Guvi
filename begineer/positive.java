115dad4cd16de25c55fe97c4c02d57f64c1c912d
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2019-03-19 15:22:27 +0530

    Third COmmit

--- begineer/positive.java
+++ begineer/positive.java
@@ -4,8 +4,11 @@ public class positive{
 	public static void main(String[] args){
 		Scanner cin=new Scanner(System.in);
 		int n=cin.nextInt();
-		if(n>=0){
+		if(n>0){
 			System.out.println("Positive");
+		}
+		else if(n==0){
+			System.out.println("Zero");
 		}
 		else{
 			System.out.println("Negative");
