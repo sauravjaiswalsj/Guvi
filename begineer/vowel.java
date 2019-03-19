2e683bdff3b31a2a24eaa4a3c163412404ea5d53
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2019-03-19 16:50:07 +0530

    added vowel

--- begineer/vowel.java
+++ begineer/vowel.java
@@ -3,8 +3,8 @@ class CheckVowel{
 	public boolean Check(char c){
 		return (c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U' );
 	}
-	public void check_vowel(String name){
-		char c=name.charAt();
+	public void check_vowel(char c){
+		//char c=name.charAt();
 		if(Check(c)){
 			System.out.println("Vowel");
 		}
@@ -19,7 +19,7 @@ class CheckVowel{
 class vowel{
 	public static void main(String[] args){
 		Scanner cin=new Scanner(System.in);
-		String name=cin.next();
+		char name=cin.next().charAt(0);
 		CheckVowel ob=new CheckVowel();
 		ob.check_vowel(name);
 	}
