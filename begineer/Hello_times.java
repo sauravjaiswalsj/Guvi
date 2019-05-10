0e227982bf7770f00dc1dbd1dd561a5405db7912
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2019-05-11 00:33:45 +0530

    Problem7

---
+++ begineer/Hello_times.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Hello_times{
+	public static void main(String[] args){
+		Scanner cin=new Scanner(System.in);
+		int n=cin.nextInt();
+		while(n!=0){
+			System.out.println("Hello");
+			n--;
+		}
+	}
+}
