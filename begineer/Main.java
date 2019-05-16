f3a546bcc6c4d5c2d6ba2ac105ce19390e57d04b
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2019-05-16 20:14:30 +0530

    set2Prob1

---
+++ begineer/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+public class Main{
+	public static void main(String[] args){
+		Scanner cin =new Scanner(System.in);
+		int a=cin.nextInt();
+		int b=cin.nextInt();
+		System.out.println((int)Math.pow(a,b));
+	}
+}
