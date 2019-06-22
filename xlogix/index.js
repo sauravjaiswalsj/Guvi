0ac11b774b4e72c67238b023e01bd7a8ed398b42
Saurav Jaiswal <sauravjaiswal999@gmail.com>
2019-06-22 14:43:18 +0530

    Add files via upload

---
+++ xlogix/index.js
@@ -0,0 +1,7 @@
+function myfunc(){
+  var email=document.getElementsById('email').value.indexOf("@");
+  var password=document.getElementsById('password').value;
+  alert(email);
+  console.log("hello");
+  console.log(email);
+}
