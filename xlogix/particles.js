b91df484c3966eef2a99b46f9837413443070570
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2019-07-31 15:16:09 +0530

    Made a page

--- xlogix/particles.js
+++ xlogix/particles.js
@@ -6,20 +6,20 @@ particlesJS("particles-js", {
       "value": 380,
       "density": {
         "enable": true,
-        "value_area": 1200
+        "value_area": 2500
       }
     },
     "color": {
       "value": "#ffffff"
     },
     "shape": {
-      "type": "polygon",
+      "type": "circle",
       "stroke": {
-        "width": 10,
+        "width": 0,
         "color": "#000000"
       },
       "polygon": {
-        "nb_sides": 4
+        "nb_sides": 5
       },
       "image": {
         "src": "img/github.svg",
@@ -48,16 +48,16 @@ particlesJS("particles-js", {
       }
     },
     "line_linked": {
-      "enable": false,
+      "enable": true,
       "distance": 150,
       "color": "#ffffff",
       "opacity": 0.4,
-      "width":50
+      "width": 1
     },
     "move": {
       "enable": true,
-      "speed": 60,
-      "direction": "right",
+      "speed": 6,
+      "direction": "none",
       "random": false,
       "straight": false,
       "out_mode": "out",
@@ -91,7 +91,7 @@ particlesJS("particles-js", {
       },
       "bubble": {
         "distance": 400,
-        "size": 400,
+        "size": 40,
         "duration": 2,
         "opacity": 8,
         "speed": 3
@@ -110,22 +110,3 @@ particlesJS("particles-js", {
   },
   "retina_detect": true
 });
-
-
-/*
-var count_particles, stats, update;
-stats = new Stats;
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function() {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-}requestAnimationFrame(update);;*/
