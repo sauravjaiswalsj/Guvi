44ee03858f6d6a4160c95fd945b9546e52635b8f
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2019-08-04 15:42:50 +0530

    Added MedianArray

--- begineer/MedianArray.java
+++ begineer/MedianArray.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class MedianArray{
+public class MedianArray{
 	public static void main(String[] args){
             Scanner cin = new Scanner(System.in);
             int n=cin.nextInt();
