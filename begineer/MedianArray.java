3858e516bf82693a00503d7d57b67490d928b1f4
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2019-08-04 15:38:01 +0530

    Added MedianArray

---
+++ begineer/MedianArray.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class MedianArray{
+	public static void main(String[] args){
+            Scanner cin = new Scanner(System.in);
+            int n=cin.nextInt();
+            int[] arr=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=cin.nextInt();
+	}
+	Arrays.sort(arr);
+	int loc=((n-1) - 0)/2;
+	int median=arr[loc];
+	System.out.println(median);
+	}
+}
+
