2e7b7211d33b450fb358e18785b1c0c87942f489
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2019-08-12 17:31:58 +0530

    Set 3 solution

--- begineer/CountLetters.java
+++ begineer/CountLetters.java
@@ -1,12 +1,13 @@
 /* @author:sauravjaiswalsj*/
 import java.util.*;
-class CountLetters{
+public class CountLetters{
 	public static void main(String[] args){
 		Scanner cin=new Scanner(System.in);
 		String s=cin.nextLine();
 		int count =0;
 		for(int i=0;i<s.length();i++){
 			if(Character.isWhitespace(s.charAt(i))==false){
+			 	
 				count++;
 			}
 		}
